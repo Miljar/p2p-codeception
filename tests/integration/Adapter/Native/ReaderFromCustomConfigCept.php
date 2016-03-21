db9aff048c025cde7a326cc9517c1592e0fab493
Tom Van Herreweghe <tom@king-foo.be>
2016-03-21 16:54:08 +0100

    Moved functional to integration tests & finished the test
    Signed-off-by: Tom Van Herreweghe <tom@king-foo.be>

---
+++ tests/integration/Adapter/Native/ReaderFromCustomConfigCept.php
@@ -0,0 +1,41 @@
+<?php
+
+use PHPExif\Adapter\Native\Mapper;
+use PHPExif\Adapter\Native\Mapper\ExifMapper;
+use PHPExif\Adapter\Native\Mapper\Exif\ApertureFieldMapper;
+use PHPExif\Adapter\Native\Mapper\IptcMapper;
+use PHPExif\Adapter\Native\Reader;
+use PHPExif\Adapter\Native\ReaderConfig;
+use PHPExif\Data\MetadataInterface;
+
+$I = new IntegrationTester($scenario);
+$I->wantTo('Create a new Native Reader instance from a custom Config');
+
+$config = new ReaderConfig(
+    false,
+    'EXIF,COMMENT'
+);
+
+$exifMapper = new ExifMapper;
+$exifMapper->registerFieldMapper(new ApertureFieldMapper);
+
+$mapper = new Mapper();
+$mapper->registerFieldMapper($exifMapper);
+$mapper->registerFieldMapper(new IptcMapper);
+
+$reader = new Reader(
+    $config,
+    $mapper
+);
+
+$data = $reader->getMetadataFromFile(
+    'https://raw.githubusercontent.com/Miljar/php-exif/master/tests/files/morning_glory_pool_500.jpg'
+);
+$this->assertTrue(
+    $data instanceof MetadataInterface
+);
+
+$this->assertEquals(
+    'f/8.0',
+    $data->getExif()->getAperture()
+);
